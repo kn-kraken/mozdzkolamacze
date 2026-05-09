e2b26aa6ee8aec005e3e9987813c72be02e5f4c8
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2026-05-09 21:25:45 +0200

    add /learn

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { BookOpen } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import navLeft from "@/public/nav-left.png";
 import navMid from "@/public/nav-mid.png";
 import navRight from "@/public/nav-right.png";
@@ -38,10 +39,13 @@ export default function RootLayout({
             <Image src={navLeft} alt="" className="h-full w-auto" />
             <div className="flex">
               <Image src={navMid} alt="" className="h-full w-auto" />
-              <button className="flex flex-col content-center w-28 p-2 pt-3 cursor-pointer rounded-lg transition-all duration-200 hover:bg-gray-100 hover:scale-105 active:scale-95">
+              <Link
+                href="/learn"
+                className="flex flex-col content-center w-28 p-2 pt-3 cursor-pointer rounded-lg transition-all duration-200 hover:bg-gray-100 hover:scale-105 active:scale-95"
+              >
                 <BookOpen className="flex-1 w-full p-1 transition-transform duration-200 group-hover:rotate-3" />
-                <label className="cursor-pointer">Learn</label>
-              </button>
+                <span className="text-center">Learn</span>
+              </Link>
             </div>
             <Image src={navRight} alt="" className="h-full w-auto" />
           </nav>
