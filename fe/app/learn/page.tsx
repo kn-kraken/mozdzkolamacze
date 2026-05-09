d9b062aa98e6ef0cc1df54d187273d20bf3922b5
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2026-05-09 21:35:54 +0200

    markdown viewer

--- fe/app/learn/page.tsx
+++ fe/app/learn/page.tsx
@@ -1,3 +1,15 @@
+import Markdown from "react-markdown";
+
+const content = `
+# Welcome to Learn
+
+This is a **markdown** viewer.
+`;
+
 export default function LearnPage() {
-  return <></>;
+  return (
+    <article className="prose max-w-3xl mx-auto p-8">
+      <Markdown>{content}</Markdown>
+    </article>
+  );
 }
