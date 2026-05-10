6b3e94a313e740b9b03a17aec82a6babbee3dd91
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2026-05-10 10:42:31 +0200

    fix Learn button

--- fe/app/components/NavLink.tsx
+++ fe/app/components/NavLink.tsx
@@ -16,7 +16,7 @@ export default function NavLink({
   return (
     <Link
       href={isActive ? "/" : href}
-      className={`flex flex-col content-center w-28 p-2 pt-3 cursor-pointer rounded-lg transition-all duration-200 hover:bg-gray-100 hover:scale-105 active:scale-95 ${
+      className={`flex flex-col content-center min-w-28 z-10 w-28 p-2 pt-3 cursor-pointer rounded-lg transition-all duration-200 bg-white hover:bg-gray-100 hover:scale-105 active:scale-95 ${
         isActive ? "bg-gray-100 font-semibold" : ""
       }`}
     >
